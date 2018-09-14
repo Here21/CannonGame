d87b8f6adbe99d4ea60b9ade0129ddddbab1160f
Here21 <deletetmr@gmail.com>
2018-09-14 17:18:27 +0800

    大炮发射炮弹（还没有做碰撞检测与炮弹状态重置）

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,5 @@
 
-function tAngle(p1, p2) {
-  const radian = Math.atan2((p2.y - p1.y), (p2.x - p1.x));
-  return radian * 180 / Math.PI - 90;
+function tAngle(target, base) {
+  const radians = Math.atan2(base.y - target.y, target.x - base.x);
+  return radians * 180 / Math.PI; // degrees
 }
--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,16 @@
   let ctx; // 画布内容
   let mx, my; // 点击坐标
   let targetAngle; // 目标角度
+  let sx, sy; // 炮弹坐标
+
 
   let letter; // 字母
   let cannon; // 炮管
+  let shell; // 炮弹
 
-  let lastframetime = 0,  diffframetime=0;  // 上一帧动画的时间，   两帧时间差
+  // 目前大多数设备的屏幕刷新率为 60 次/秒，所以通常来讲 fps 为 60 frame/s 时动画效果最好，也就是每帧的消耗时间为 16.67ms。
+  let fps = 0;
+  let lastframetime = 0,  frametime=0;  // 上一帧动画的时间，   两帧时间差
 
 
   window.G = {}; // 全局对象
@@ -34,6 +39,10 @@
     cannon = new CannonObject();
     cannon.init();
 
+    // new 炮弹类
+    shell = new ShellObject();
+    shell.init();
+
     // 事件
     canvas.addEventListener("click", (event) => {
       mx = event.offsetX;
@@ -47,8 +56,10 @@
   // 帧动画
   G.igniteLoop = (timestamp) => {
     // 每帧耗时
-    diffframetime = timestamp - lastframetime;
+    frametime = timestamp - lastframetime;
     lastframetime = timestamp;
+    fps = Math.round(1000 / frametime);
+
     // requestAnimationFrame 的 callback中参数表示当前触发回调时当前的时间
     window.requestAnimationFrame(G.igniteLoop);
     // setInterval(G.igniteLoop, )
@@ -58,6 +69,7 @@
 
     // 绘制画布内容
     letter.update();
+    shell.update();
     cannon.update();
 
   };
@@ -99,11 +111,11 @@
         // 判断鼠标点击区域
         if (ctx.isPointInPath(mx, my)) {
           console.log('no is click', i);
-          const angle = tAngle({x: this.x[i], y: this.y[i]}, { x: cannon.tx, y: cannon.ty});
-          // 判断旋转角度，以cannon 的位置来看，cannon.tx 右边是正角度旋转，左边是负角度旋转
-          targetAngle = angle;
-          // console.log("------", angle, '====', targetAngle);
-          // console.log("position", cannon.tx, cannon.ty, this.x[i], this.y[i]);
+          // 计算偏角
+          targetAngle = tAngle({x: this.x[i], y: this.y[i]}, { x: cannon.tx, y: cannon.ty});
+          //                     速度     *    时间 = 距离 /  帧率
+          shell.x = shell.x + shell.speed * frametime / fps * Math.cos(targetAngle * Math.PI / 180);
+          shell.y = shell.y - shell.speed * frametime / fps * Math.sin(targetAngle * Math.PI / 180);
         }
       }
     }
@@ -137,55 +149,43 @@
     // 绘制
     ctx.save();
     ctx.translate(this.x + this.img.width / 2, this.y + this.img.height);
-    // console.log(targetAngle);
-    ctx.rotate(targetAngle * Math.PI / 180);  // rotate() 参数是弧度为单位的值
+    // 炮管朝向与坐标系Y轴是反的，旋转角度是90 - targetAngle的夹角
+    ctx.rotate((90 - targetAngle) * Math.PI / 180);  // rotate() 参数是弧度为单位的值
     ctx.drawImage(this.img, -this.img.width / 2, -this.img.height);
     ctx.restore();
   };
 
   // 定义炮弹类
   const ShellObject = function() {
-    this.speed = 20;
-    this.x = cannon.tx;
-    this.y = cannon.ty;
+    this.speed = 50;
+    this.x = 0;
+    this.y = 0;
     this.radius = 25;
-    this.margin = 20;
-    this.letter = [];
-    this.alive = [];  // boolean 是否可用
+    this.letter = '';
+    this.alive = false;  // boolean 是否可用
   }
   ShellObject.prototype.init = function() {
-    // 计算左偏移值，近似居中。anchor 在 x / 2, y / 2
-    let offset;
-    if (canvasWidth < (this.num - 1) * this.radius * 2 + (this.margin * (this.num - 1))) {
-      offset = this.radius;
-    } else {
-      offset = (canvasWidth - ((this.num - 1) * this.radius * 2 + (this.margin * (this.num - 1)))) / 2;
-    }
-    for(let i = 0; i < this.num; i++){
-      this.x[i] = this.radius * 2 * i + this.margin * i + offset; // 居中
-      this.y[i] = 100; // 固定高度
-      this.alive[i] = true;   //初始值都为false
-    }
+    this.x = cannon.tx;
+    this.y = cannon.ty;
+    this.alive = true;
   }
   ShellObject.prototype.update = function() {
-    for(let i =0;i< this.num; i++){
-      if(this.alive[i]){
-        ctx.beginPath();
-        ctx.arc(this.x[i], this.y[i], this.radius, 0, Math.PI * 2, true);
-        ctx.closePath();
-        ctx.fillStyle = 'rgb(255,255,255)';
-        ctx.fill();
+    if(this.alive){
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+      ctx.closePath();
+      ctx.fillStyle = 'rgb(255,255,255)';
+      ctx.fill();
 
-        // 判断鼠标点击区域
-        if (ctx.isPointInPath(mx, my)) {
-          console.log('no is click', i);
-          const angle = tAngle({x: this.x[i], y: this.y[i]}, { x: cannon.tx, y: cannon.ty});
-          // 判断旋转角度，以cannon 的位置来看，cannon.tx 右边是正角度旋转，左边是负角度旋转
-          targetAngle = angle;
-          // console.log("------", angle, '====', targetAngle);
-          // console.log("position", cannon.tx, cannon.ty, this.x[i], this.y[i]);
-        }
-      }
+      // // 判断鼠标点击区域
+      // if (ctx.isPointInPath(mx, my)) {
+      //   console.log('no is click', i);
+      //   const angle = tAngle({x: this.x[i], y: this.y[i]}, { x: cannon.tx, y: cannon.ty});
+      //   // 判断旋转角度，以cannon 的位置来看，cannon.tx 右边是正角度旋转，左边是负角度旋转
+      //   targetAngle = angle;
+      //   // console.log("------", angle, '====', targetAngle);
+      //   // console.log("position", cannon.tx, cannon.ty, this.x[i], this.y[i]);
+      // }
     }
   }
 
